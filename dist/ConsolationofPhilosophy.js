e9f15097bf2dc68717a0836a1ff78deae3262f28
CenYee <cemselee@outlook.com>
2021-10-24 10:18:07 +0800

    Site updated: 2021-10-24 10:18:06

--- dist/ConsolationofPhilosophy.js
+++ dist/ConsolationofPhilosophy.js
@@ -1,11 +1,17 @@
 const apConsolationofPhilosophy = new APlayer({
     container: document.getElementById('aplayerConsolationofPhilosophy'),
+    fixed: false,
+    listFolded: false,
+    listMaxHeight: 90,
+    autoplay: false,
+    lrcType: 1,
     audio: [
 	{
         name: "哲学的安慰",
         artist: '倪湛舸',        
         url: 'https://cdn.jsdelivr.net/gh/CenYee/House/files/TheConsolationofPhilosophy.mp3',
-        cover: 'https://cdn.jsdelivr.net/gh/CenYee/House/articleimage/TheConsolationofPhilosophy.png'
+        cover: 'https://cdn.jsdelivr.net/gh/CenYee/House/articleimage/TheConsolationofPhilosophy.png',
+        lrc: 'https://cdn.jsdelivr.net/gh/CenYee/House/files/TheConsolationofPhilosophy.lrc'
       }
       ]
 });
--- dist/Remnants.js
+++ dist/Remnants.js
@@ -1,11 +1,17 @@
 const apRemnants = new APlayer({
     container: document.getElementById('aplayerRemnants'),
+    fixed: false,
+    listFolded: false,
+    listMaxHeight: 90,
+    autoplay: false,
+    lrcType: 3,
     audio: [
 	{
         name: "Remnants",
         artist: 'Kazukii',        
         url: 'https://cdn.jsdelivr.net/gh/CenYee/House/files/Remnants.mp3',
-        cover: 'https://cdn.jsdelivr.net/gh/CenYee/House/articleimage/Remnants.jpg'
+        cover: 'https://cdn.jsdelivr.net/gh/CenYee/House/articleimage/Remnants.jpg',
+        lrc: 'https://cdn.jsdelivr.net/gh/CenYee/House/files/Remnants.lrc'
       }
       ]
 });
